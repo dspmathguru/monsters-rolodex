ce8e76c19ceea44e91a21bc0af7e789c219f85bf
Richard J. Tobias <rjjt@cephasonics.com>
2019-10-09 23:16:50 +0100

    Updates

---
+++ precache-manifest.5b4d2d5377f043969e5c77f2bc065ff1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c3c0ba4768752fd9dbba8cad834037a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9868b80f0969a1ba2f4a",
+    "url": "/monsters-rolodex/static/css/main.535eb2a3.chunk.css"
+  },
+  {
+    "revision": "b0703eff4582a78f493a",
+    "url": "/monsters-rolodex/static/js/2.5075ebff.chunk.js"
+  },
+  {
+    "revision": "9868b80f0969a1ba2f4a",
+    "url": "/monsters-rolodex/static/js/main.fa17dcdd.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
